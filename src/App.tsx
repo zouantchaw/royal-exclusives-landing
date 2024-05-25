d1210f79e8d9fef142f31aab5e479656c784b1ba
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2024-05-25 18:31:09 -0400

    Update component imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { About } from "./components/About";
-import { Cta } from "./components/Cta";
 import { FAQ } from "./components/FAQ";
-import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
-import { AlFakher } from "./components/alfakher";
+import { AlFakher } from "./components/AlFakher";
 import { AlFakherMixes } from "./components/AlFakherMixes";
 import { Adalya } from "./components/Adalya";
 import { Navbar } from "./components/Navbar";
 import { Newsletter } from "./components/Newsletter";
-import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
-import { Services } from "./components/Services";
 import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 
